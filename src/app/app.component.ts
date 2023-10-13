29e040e78f53379dd97fbff818746bd25c866fb8
KEERTHANA-E <100776925+KEERTHANA-E@users.noreply.github.com>
2023-10-13 13:42:47 +0530

    files uploaded

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstProject';
+  curIdx = 0;
+  images = [
+    "https://images.unsplash.com/photo-1696280292021-76c29fb11f99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+    "https://images.unsplash.com/photo-1696943444991-def9485f89f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    "https://images.unsplash.com/photo-1682686579688-c2ba945eda0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    "https://images.unsplash.com/photo-1696928621163-60b2dae656be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+  ]  
+  indexes =[0,1,2,3];
+  moveLeft() {
+    if(this.curIdx==0){
+      this.curIdx=3;
+    }
+    else {
+      this.curIdx--;
+    }
+    console.log(this.curIdx);
+  }
+  moveRight(){
+    if(this.curIdx==3){
+      this.curIdx=0;
+    }
+    else{
+      this.curIdx++;
+    }
+  }
+}
